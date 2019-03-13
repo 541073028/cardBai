94cdd27249483b03e864c315fff5f09fe56bd12f
164197904@qq.com <164197904@qq.com>
2019-03-13 16:31:29 +0800

    更新信息

--- src/components/invite/common.vue
+++ src/components/invite/common.vue
@@ -1,12 +1,22 @@
 <template>
     <div class="common">
-        123
+        <img src="../../assets/img/invite-common@2x.jpg" alt="">
+        <div class="notice">
+            恭喜{{user}}用户{{phone}}累计获得 <span>{{money}}元</span>邀请奖励
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "common"
+        name: "common",
+        data(){
+            return{
+                user: '**',//notice用户
+                phone: '158****1797',//notice手机号
+                money: '20',//notice金额
+            }
+        }
     }
 </script>
 
@@ -14,6 +24,29 @@
  .common
      width: 7.5rem
      height: auto
-     background: url("../../assets/img/invite-common@2x.jpg")
-     background-size: 7.5rem auto
+     position: relative
+     top: -1.28rem
+     left: 0
+     img
+         width: 100%
+         height: auto
+         position: absolute
+         top: 0
+         left: 0
+     .notice
+         width: 6.16rem
+         height: .59rem
+         line-height: .59rem
+         background: rgba(0,0,0,.1)
+         font-size: .24rem
+         font-family: 'PingFang-SC-Bold'
+         border-radius: .3rem
+         color: #fff
+         position: absolute
+         top: 5.75rem
+         left: 50%
+         margin-left: -3.08rem
+         text-align: center
+         span
+            color: #FFF100
 </style>
--- src/views/invite/invite.vue
+++ src/views/invite/invite.vue
@@ -1,5 +1,9 @@
 <template>
     <div class="invite">
+        <div class="title">
+            <img src="../../assets/img/fanhui@2x.png" alt="">
+            <p>邀请好友</p>
+        </div>
         <!--普通common-->
         <common></common>
 
@@ -17,5 +21,25 @@
 </script>
 
 <style scoped lang="sass">
+.invite
+    .title
+        width: 100%
+        //height: .88rem
+        background: #fff
+        font-size: .34rem
+        font-family: 'PingFang-SC-Bold'
+        font-weight: bold
+        color: rgba(62,62,77,1)
+        line-height: .88rem
+        position: relative
+        text-align: center
+        z-index: 99
+        img
+            width: .45rem
+            height: .45rem
+            position: absolute
+            top: 50%
+            left: .4rem
+            margin-top: -.225rem
 
 </style>
