95d11fe7cc22d8216df8d219a01d39b6b591160d
164197904@qq.com <164197904@qq.com>
2019-03-13 15:17:03 +0800

    1

---
+++ src/components/invite/common.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="common">
+        123
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "common"
+    }
+</script>
+
+<style scoped lang="sass">
+ .common
+     width: 7.5rem
+     height: auto
+     background: url("../../assets/img/invite-common@2x.jpg")
+     background-size: 7.5rem auto
+</style>
---
+++ src/views/invite/invite.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="invite">
+        <!--普通common-->
+        <common></common>
+
+    </div>
+</template>
+
+<script>
+    import common from '@/components/invite/common.vue'
+    export default {
+        name: "invite",
+        components: {
+            common,
+        }
+    }
+</script>
+
+<style scoped lang="sass">
+
+</style>
